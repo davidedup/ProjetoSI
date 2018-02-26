fa687fe58c697581a51444cf4bc403b5ad03124b
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-25 21:17:19 -0300

    ProdutosRepository movido pra package repositories

--- src/main/java/com/ufcg/si1/repositories/ProdutosRepository.java
+++ src/main/java/com/ufcg/si1/repositories/ProdutosRepository.java
@@ -1,10 +1,10 @@
-package com.ufcg.si1;
+package com.ufcg.si1.repositories;
 
 import com.ufcg.si1.model.Produto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutosRepository extends CrudRepository<Produto, Long> {
 
-    
+
 
 }
