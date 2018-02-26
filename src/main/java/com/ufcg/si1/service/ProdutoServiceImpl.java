1762dd1aaa1565a4a4e71feca996bc098edc9f76
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-26 20:48:49 -0300

    ProdutoServiceImpl de acordo com persistência

--- src/main/java/com/ufcg/si1/service/ProdutoServiceImpl.java
+++ src/main/java/com/ufcg/si1/service/ProdutoServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.ufcg.si1.repositories.ProdutosRepository;
+import com.ufcg.si1.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ufcg.si1.model.Lote;
@@ -13,88 +16,47 @@ import com.ufcg.si1.model.Produto;
 @Service("produtoService")
 public class ProdutoServiceImpl implements ProdutoService {
 
-	private static final AtomicLong counter = new AtomicLong();
-
-	private static List<Produto> produtos;
-	
-	private static List<Lote> lotes;
-
-	static {
-		produtos = populateDummyProdutos();
-		lotes = new ArrayList<>();
-	}
-
-	private static List<Produto> populateDummyProdutos() {
-		List<Produto> produtos = new ArrayList<Produto>();
-		
-		produtos.add(new Produto(counter.incrementAndGet(), "Leite Integral", "87654321-B", "Parmalat", "Mercearia"));
-		produtos.add(new Produto(counter.incrementAndGet(), "Arroz Integral", "87654322-B", "Tio Joao", "Perecíveis"));
-		produtos.add(new Produto(counter.incrementAndGet(), "Sabao em Po", "87654323-B", "OMO", "Limpeza"));
-		produtos.add(new Produto(counter.incrementAndGet(), "Agua Sanitaria", "87654324-C", "Dragao", "limpesa"));
-		produtos.add(new Produto(counter.incrementAndGet(), "Creme Dental", "87654325-C", "Colgate", "HIGIENE"));
-
-		return produtos;
-	}
+	@Autowired
+	private ProdutosRepository produtosRepository;
 
 	public List<Produto> findAllProdutos() {
-		return produtos;
+		Iterable<Produto> produtos = this.produtosRepository.findAll();
+		List<Produto> produtosList = Util.toList(produtos);
+		return produtosList;
 	}
 
+
 	public void saveProduto(Produto produto) {
-		produto.mudaId(counter.incrementAndGet());
-		produtos.add(produto);
+		this.produtosRepository.save(produto);
 	}
 
 	public void updateProduto(Produto produto) {
-		int index = produtos.indexOf(produto);
-		produtos.set(index, produto);
+		this.produtosRepository.save(produto);
 	}
 
 	public void deleteProdutoById(long id) {
-
-		for (Iterator<Produto> iterator = produtos.iterator(); iterator.hasNext();) {
-			Produto p = iterator.next();
-			if (p.getId() == id) {
-				iterator.remove();
-			}
-		}
-	}
-
-	// este metodo nunca eh chamado, mas se precisar estah aqui
-	public int size() {
-		return produtos.size();
+		this.produtosRepository.delete(id);
 	}
 
 	public Iterator<Produto> getIterator() {
-		return produtos.iterator();
+		Iterable<Produto> produtos = this.produtosRepository.findAll();
+		Iterator iterator = produtos.iterator();
+
+		return iterator;
 	}
 
 	public void deleteAllUsers() {
-		produtos.clear();
+		this.produtosRepository.deleteAll();
 	}
 
 	public Produto findById(long id) {
-		for (Produto produto : produtos) {
-			if (produto.getId() == id) {
-				return produto;
-			}
-		}
-		return null;
+		Produto produto = this.produtosRepository.getProdutoById(id);
+		return produto;
 	}
 
 	public boolean doesProdutoExist(Produto produto) {
-		for (Produto p : produtos) {
-			if (p.getCodigoBarra().equals(produto.getCodigoBarra())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public Lote saveLote(Lote lote) {
-		lote.setId(counter.incrementAndGet());
-		lotes.add(lote);
-
-		return lote;
+		long id = produto.getId();
+		boolean produtoExiste = this.produtosRepository.exists(id);
+		return produtoExiste;
 	}
 }
