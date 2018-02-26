5476c17fd02ff90148493ea342c4ffd142dbebf5
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-25 21:18:22 -0300

    criado LotesRepository

---
+++ src/main/java/com/ufcg/si1/repositories/LotesRepository.java
@@ -0,0 +1,10 @@
+package com.ufcg.si1.repositories;
+
+import com.ufcg.si1.model.Lote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LotesRepository extends CrudRepository<Lote, Long> {
+
+    
+
+}
