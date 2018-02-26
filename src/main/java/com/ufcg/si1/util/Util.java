76dac2df6b27b9f5cd668c4f23a542ce94ab7d76
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-26 20:46:57 -0300

    criada classe Util

---
+++ src/main/java/com/ufcg/si1/util/Util.java
@@ -0,0 +1,19 @@
+package com.ufcg.si1.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Util {
+
+    public static <T> List<T> toList(Iterable<T> iterable) {
+        ArrayList<T> list = new ArrayList<T>();
+
+        for(T t: iterable) {
+            list.add(t);
+        }
+
+        return list;
+    }
+
+}
