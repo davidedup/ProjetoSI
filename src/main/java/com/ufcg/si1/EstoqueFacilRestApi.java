a59e0539c68f4c102664e6e8d5d0243cff6a3bce
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-25 21:25:25 -0300

    adicionada configuração de CORS

--- src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
+++ src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
@@ -2,6 +2,9 @@ package com.ufcg.si1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication(scanBasePackages={"com.ufcg.si1"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
@@ -10,4 +13,14 @@ public class EstoqueFacilRestApi {
 	public static void main(String[] args) {
 		SpringApplication.run(EstoqueFacilRestApi.class, args);
 	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
+
 }
