db84145721fa7df37c3a5fcd4eb602befabb633a
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-25 21:26:56 -0300

    detalhe q faltou para a config do CORS

--- src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
+++ src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
@@ -2,6 +2,7 @@ package com.ufcg.si1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,6 +15,7 @@ public class EstoqueFacilRestApi {
 		SpringApplication.run(EstoqueFacilRestApi.class, args);
 	}
 
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurerAdapter() {
 			@Override
