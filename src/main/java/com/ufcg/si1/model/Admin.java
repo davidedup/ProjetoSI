69d8e547b55ea701fb546a3af01855caa0e6237f
matheustargino <matheus.nyo@gmail.com>
2018-03-11 19:13:12 -0300

    javadoc

--- src/main/java/com/ufcg/si1/model/Admin.java
+++ src/main/java/com/ufcg/si1/model/Admin.java
@@ -23,24 +23,54 @@ public class Admin {
 		login = "";
 	}
 	
+	/**
+         * Construtor da classe Admin
+         * 
+         * @param login login do Admin
+         * @param nome nome do admin
+         * @param senha senha do admin
+         */
 	public Admin(String login, String nome, String senha) {
 		this.login = login;
 		this.nome = nome;
 		this.senha = senha;
 	}
-
+        
+        /**
+         * Retorna o login do admin
+         * 
+         * @return login do Admin
+         */
+        
 	public String getLogin() {
 		return this.login;
 	}
-
+        
+        /**
+         * Altera o login do Admin
+         * 
+         * @param login para ser alterado
+         */
+        
 	public void setLogin(String login) {
 		this.login = login;
 	}
-
+        
+        /**
+         * Get do nome do admin
+         *
+         * @return nome do admin
+         */
+        
 	public String getNome() {
 		return this.nome;
 	}
-
+        
+        /**
+         * Altera o nome do usuário
+         * 
+         * @param nome para ser alterado
+         */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -48,9 +78,14 @@ public class Admin {
 	public String getSenha() {
 		return this.senha;
 	}
-
+        /**
+         * Altera a senha do admin
+         * 
+         * @param senha a ser alterada
+         */
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
 }
+
