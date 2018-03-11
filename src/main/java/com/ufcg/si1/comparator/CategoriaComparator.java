3ec4ee4d7d992d2f2e5fad6f49b74b9b286b5247
matheustargino <matheus.nyo@gmail.com>
2018-03-11 14:47:00 -0300

    javadoc

--- src/main/java/com/ufcg/si1/comparator/CategoriaComparator.java
+++ src/main/java/com/ufcg/si1/comparator/CategoriaComparator.java
@@ -7,6 +7,14 @@ import java.util.Comparator;
 
 public class CategoriaComparator implements Comparator<Produto> {
     
+     /**
+     * Compara as categorias dos produtos
+     * 
+     * @param produto1 produto 1 onde a categoria será comparado
+     * @param produto2 produto 2 onde a categoria será comparado
+     * @return retorna 0 se as categorias forem iguais, qualquer outro valor para categorias diferentes
+     */
+	
 	@Override
     public int compare(Produto produto1, Produto produto2) {
         Categoria categoria1 = produto1.getCategoria();
