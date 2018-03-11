1318ae0e03f16e240ad7b1d55ed223000f07db5b
matheustargino <matheus.nyo@gmail.com>
2018-03-11 18:22:02 -0300

    javadoc

--- src/main/java/com/ufcg/si1/comparator/PrecoComparator.java
+++ src/main/java/com/ufcg/si1/comparator/PrecoComparator.java
@@ -7,6 +7,14 @@ import java.util.Comparator;
 
 public class PrecoComparator implements Comparator<Produto> {
     
+	/**
+        * Compara os preços dos produtos
+        * 
+        * @param produto1 produto 1 onde o preço será comparado
+        * @param produto2 produto 2 onde o preço será comparado
+        * @return retorna 0 se os preços forem iguais, qualquer outro valor se forem diferentes
+        */
+	
 	@Override
 	public int compare(Produto produto1, Produto produto2) {
         BigDecimal preco1 = produto1.getPreco();
