03be676052fd23c1f8ca2aa83a7ff8f217757e45
matheustargino <matheus.nyo@gmail.com>
2018-03-11 18:20:51 -0300

    javadoc

--- src/main/java/com/ufcg/si1/comparator/NomeComparator.java
+++ src/main/java/com/ufcg/si1/comparator/NomeComparator.java
@@ -4,6 +4,14 @@ import com.ufcg.si1.model.Produto;
 
 import java.util.Comparator;
 
+     /**
+     * Compara os nomes dos produtos
+     * 
+     * @param produto1 produto 1 onde o nome será comparado
+     * @param produto2 produto 2 onde o nome será comparado
+     * @return retorna 0 se os nomes forem iguais, qualquer outro valor se forem diferentes
+     */
+
 public class NomeComparator implements Comparator<Produto> {
     
 	@Override
